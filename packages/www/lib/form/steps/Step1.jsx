c2b8218e90994874a9b76830cd0335713d26f5dd
Johan Öbrink <johan.obrink@gmail.com>
2020-03-23 16:54:55 +0100

    Some cleanup before rebooting with SEB comps.

--- packages/www/lib/form/steps/Step1.jsx
+++ packages/www/lib/form/steps/Step1.jsx
@@ -1,5 +1,4 @@
-import React, { useContext, useState } from 'react'
-import { Accordion } from '@sebgroup/react-components/dist/Accordion'
+import React, { useContext } from 'react'
 import { Button } from '@sebgroup/react-components/dist/Button'
 import FormContext from '../FormContext'
 import { actions } from '../constants'
@@ -7,37 +6,15 @@ import { LineOfBusiness, Revenue, NumberOfEmployees } from '../questions'
 
 const Step1 = () => {
   const {dispatch} = useContext(FormContext)
-  const [index, setIndex] = useState(0)
-  const accordionListItems = [
-    {
-      content: <>
-        <LineOfBusiness />
-        <Button onClick={() => setIndex(1)}>Next</Button>
-      </>
-    }
-  ]
+  
   return (
-    <Accordion activeIndex={index} list={accordionListItems} />
+    <>
+      <LineOfBusiness />
+      <Revenue />
+      <NumberOfEmployees />
+      <Button theme="primary" onClick={() => dispatch({type: actions.ANSWER})}>Next</Button>
+    </>
   )
 }
-
-/*
-    <Accordion>
-      <Accordion.Title>Start</Accordion.Title>
-      <Accordion.Content active={index===0}>
-        <LineOfBusiness />
-        <Button onClick={() => setIndex(1)}>Next</Button>
-      </Accordion.Content>
-      <Accordion.Content active={index===1}>
-        <Revenue />
-        <Button onClick={() => setIndex(2)}>Next</Button>
-      </Accordion.Content>
-      <Accordion.Content active={index===2}>
-        <NumberOfEmployees />
-        <Button onClick={() => setIndex(3)}>Next</Button>
-      </Accordion.Content>
-      <Accordion.Content active={index===3}><Button onClick={() => dispatch({type: actions.ANSWER})}>Next</Button></Accordion.Content>
-    </Accordion>
-*/
 
 export default Step1
--- packages/www/lib/form/steps/Step2.jsx
+++ packages/www/lib/form/steps/Step2.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Button } from '@sebgroup/react-components/dist/Button'
+import FormContext from '../FormContext'
+import { actions } from '../constants'
 import { ShortTermLayoffs, SickLeave, Deferral } from '../questions'
 
 const Step2 = () => {
+  const {dispatch} = useContext(FormContext)
+  
   return (
     <>
       <h2>Step 2</h2>
       <ShortTermLayoffs />
       <SickLeave />
       <Deferral />
+      <Button theme="primary" onClick={() => dispatch({type: actions.ANSWER})}>Next</Button>
     </>
   )
 }
