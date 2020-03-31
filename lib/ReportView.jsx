2a00d5a5704b70c83d999c76425618aa56a9efd0
Johan Öbrink <johan.obrink@gmail.com>
2020-03-31 21:49:08 +0200

    feat: 🎸 Now loads reports (#25)

--- lib/ReportView.jsx
+++ lib/ReportView.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import LoadReport from './report/LoadReport'
+import Report from './report/Report'
 
 export default () => {
   const { reportId } = useParams()
   return (
-    <div>Report {reportId}</div>
+    <div>
+      <h1>Report</h1>
+      { reportId && <Report reportId={reportId} /> }
+      { !reportId && <LoadReport /> }
+    </div>
   )
 }
