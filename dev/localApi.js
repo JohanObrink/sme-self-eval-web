e0921350b9cb24a1057efb2925887c2ad5f024fb
Johan Öbrink <johan.obrink@gmail.com>
2020-03-31 17:05:30 +0200

    feat: 🎸 Added api for simple CRU(-D)

--- dev/localApi.js
+++ dev/localApi.js
@@ -1,4 +1,6 @@
 const app = require('sme-self-eval-api/lib/app')
+const { logger } = require('sme-self-eval-api/lib/logging')
+
 app.listen(1337, () => {
-  console.log('Local API listening on 1337')
+  logger.info('Local API listening on 1337')
 })
