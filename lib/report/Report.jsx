52885d150195b71955db640d34ccc0e3e4a642ed
Joel Lundgren <joel.lundgren@seb.se>
2020-04-03 15:57:14 +0200

    feat: 🎸 Updated report page
    Icons and more

--- lib/report/Report.jsx
+++ lib/report/Report.jsx
@@ -1,65 +1,114 @@
-import React, { useEffect, useState } from "react";
-import { get } from "../api";
-import { form } from "../form/data";
+import React, { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faTimes, faMinus } from '@fortawesome/free-solid-svg-icons'
+import { get } from '../api'
+import { form } from '../form/data'
 
 export default ({ reportId }) => {
-  const [loading, setLoading] = useState();
-  const [report, setReport] = useState();
-  const [error, setError] = useState();
+  const [loading, setLoading] = useState()
+  const [report, setReport] = useState()
+  const [error, setError] = useState()
   useEffect(() => {
-    setReport();
-    setError();
-    setLoading(true);
+    setReport()
+    setError()
+    setLoading(true)
 
     get(reportId)
       .then(report => setReport(report))
       .catch(error => setError(error))
-      .finally(() => setLoading(false));
-  }, [reportId]);
+      .finally(() => setLoading(false))
+  }, [reportId])
 
-  const rows = [];
+  const rows = []
+
+  const iconRender = (value) => {
+    switch(value) {
+      case "ja":
+        return <FontAwesomeIcon icon={faCheck} />
+        break;
+      case "nej":
+        return <FontAwesomeIcon icon={faTimes} />
+        break;
+      case "ignore":
+        return <FontAwesomeIcon icon={faMinus} />
+        break;
+      default:
+        return <FontAwesomeIcon icon={faMinus} />
+        break;
+
+    }
+  }
 
   for (let i = 0; i < form.steps.length; i++) {
-    var step = form.steps[i];
+    var step = form.steps[i]
     rows.push(
       <tr key={i}>
-        <td colSpan="2" key={step.headline}>
-          <b>{step.headline}</b>
-        </td>
+        <th colSpan='2' key={step.headline}>{step.headline}</th>
       </tr>
-    );
-    let children = [];
+    )
+    let children = []
     for (let x = 0; x < step.questions.length; x++) {
-      var q = step.questions[x];
+      var q = step.questions[x]
       if (report && report[q.path]) {
         rows.push(
           <tr key={q.suggestion}>
-            <td>{report[q.path]}</td>
+            <td>{iconRender(report[q.path])}</td>
             <td>{q.suggestion}</td>
           </tr>
-        );
+        )
       }
     }
   }
 
   return (
-    <div>
-      {loading && <div>Loading...</div>}
-      {error && (
-        <>
-          <h4>Error</h4>
-          <pre>{error.toString()}</pre>
-        </>
-      )}
+    <>
+      <div className='row'>
+        <div className='col'>
+          {loading && <div>Läser in...</div>}
+
+          {error && (
+            <>
+              <h4>Ett fel inträffade</h4>
+              <pre>{error.toString()}</pre>
+            </>
+          )}
+        </div>
+      </div>
       {report && (
         <>
-          <h4>Report</h4>
-          <pre>{JSON.stringify(report)}</pre>
-          <table>
-            <tbody>{rows}</tbody>
-          </table>
+          <div className='row report-explanation'>
+            <div className='col'>
+              <div className='row'>
+                <div className='col-4'>
+                  <div>Teckenförklaring</div>
+                </div>
+              </div>
+              <div className='row justify-content-start'>
+                <div className='col-2 col-sm-2 col-md-2'>
+                  <div>
+                    <FontAwesomeIcon icon={faCheck} /> Ja
+                  </div>
+                </div>
+                <div className='col-2 col-sm-2 col-md-2'>
+                  <div>
+                    <FontAwesomeIcon icon={faTimes} /> Nej
+                  </div>
+                </div>
+                <div className='col-8 col-sm-8 col-md-8'>
+                  <FontAwesomeIcon icon={faMinus} /> Inte aktuellt / Inget svar
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col report-table'>
+              <table className='table'>
+                <tbody>{rows}</tbody>
+              </table>
+            </div>
+          </div>
         </>
       )}
-    </div>
-  );
-};
+    </>
+  )
+}
