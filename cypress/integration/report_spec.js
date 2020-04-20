e801658f8b3b9f715ce17a827ab95f4bee53e86c
Johan Öbrink <johan.obrink@gmail.com>
2020-04-20 11:50:44 +0200

    test: 💍 Added Cypress tests

---
+++ cypress/integration/report_spec.js
@@ -0,0 +1,45 @@
+/// <reference types="cypress" />
+
+describe('Report', () => {
+  let reportUrl
+  before(() => {
+    cy.visit('/test')
+    // Step 1
+    cy.question(0).answer(0)
+    cy.question(1).answer(1)
+    cy.question(2).answer(2)
+    cy.question(3).answer(0)
+    cy.question(4).answer(1)
+    cy.next()
+    // Step 2
+    cy.question(0).answer(0)
+    cy.question(1).answer(1)
+    cy.question(2).answer(2)
+    cy.question(3).answer(0)
+    cy.next()
+    // Step 3
+    cy.question(0).answer(0)
+    cy.question(1).answer(1)
+    cy.question(2).answer(2)
+    cy.next()
+    // Step 4
+    cy.question(0).answer(0)
+    cy.question(1).answer(1)
+    cy.question(2).answer(2)
+    cy.next()
+    // Step 5
+    cy.question(0).answer(0)
+    cy.question(1).answer(1)
+    cy.question(2).answer(2)
+    cy.question(3).answer(0)
+    cy.finish()
+
+    cy.url().then(url => reportUrl = url)
+  })
+  beforeEach(() => {
+    cy.visit(reportUrl)
+  })
+  it('Shows the correct headline', () => {
+    cy.get('h1').contains('Checklista – förslag på åtgärder')
+  })
+})
