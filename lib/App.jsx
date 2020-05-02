301369403f3f317102090bf8c868bbd758ce245e
Leopold Roos <leopold.roos@seb.se>
2020-05-02 20:02:17 +0200

    refactor: 💡 moved most text

--- lib/App.jsx
+++ lib/App.jsx
@@ -4,16 +4,17 @@ import IntroView from './IntroView'
 import UserView from './UserView'
 import ReportView from './ReportView'
 import PageTracker from './components/PageTracker'
+import { getText } from './form/appTexts'
 
 export default () => {
   return (
     <Router>
       <nav className="navbar navbar-light bg-light">
         <a href="https://seb.se/">
-          <div className="navbar-brand">Självtest</div>
+          <div className="navbar-brand">{getText("label_navleft")}</div>
         </a>
         <a href="/">
-          <span className="navbar-text float-left">Testa din verksamhet</span>
+          <span className="navbar-text float-left">{getText("label_navright")}</span>
         </a>
       </nav>
       <PageTracker>
--- lib/IntroView.jsx
+++ lib/IntroView.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { Button } from '@sebgroup/react-components/dist/Button/Button'
 import { TextBox } from '@sebgroup/react-components/dist/TextBox/TextBox'
 import faArrowRight from './assets/arrow-right.svg'
+import { getText } from './form/appTexts'
 
 export default () => {
   const [reportId, setReportId] = useState('')
@@ -11,46 +12,26 @@ export default () => {
     <div className="container intro-page">
       <div className="row">
         <div className="col-12">
-          <p>Senast uppdaterad 4 april 2020.</p>
+          <p>{getText("label_last update")}</p>
         </div>
         <div className="col-12">
-          <h1>Hur påverkas din verksamhet av coronautbrottet?</h1>
+          <h1>{getText("heading_howaffected")}</h1>
         </div>
       </div>
       <div className="row">
         <div className="col-12 order-first order-sm-first">
-          <h3>
-            I tider av kris är det viktigt att få en överblick över ditt företags situation. Med det
-            här självskattningsverktyget vill vi ge dig bättre koll på tänkbara åtgärder som du
-            själv kan vidta och vilken hjälp du kan få från oss och myndigheter.
-          </h3>
+          <h3>{getText("text_intro")}</h3>
         </div>
-        <div className="col-md-6 order-second order-sm-second">
-          <p>
-            Frågorna är generella och alla kanske inte gäller din verksamhet. När du har svarat på
-            frågorna får du en sammanställning med dina svar och förslag på åtgärder.
-          </p>
-          <p>
-            Uppgifterna du fyller i är enbart tillgänglig via koden du får när du är färdig, så se
-            till att notera den. Vi behandlar dina uppgifter enligt{' '}
-            <a
-              href="https://seb.se/juridik-och-sakerhet/sebs-integritetspolicy"
-              target="blank"
-              className="external"
-            >
-              vår integritetspolicy.
-            </a>
-          </p>
-        </div>
+        <div className="col-md-6 order-second order-sm-second" dangerouslySetInnerHTML={{__html: getText("text_description")}} />
         <div className="col-md-6 order-4 order-md-3 previous-test">
           <div className="row">
             <div className="col-12 align-self-start">
-              <h4>Har du redan gjort testet?</h4>
+              <h4>{getText("label_alreadydone")}</h4>
             </div>
             <div className="col-6">
               <div className="row">
                 <div className="col-12 strong align-self-start">
-                  <p>Fyll i din kod.</p>
+                  <p>{getText("label_fillcode")}</p>
                 </div>
                 <div className="col">
                   <TextBox
@@ -65,7 +46,7 @@ export default () => {
               <div className="row">
                 <div className="col strong align-self-start strong">
                   <Link to={{ pathname: `/report/${reportId}` }}>
-                    Hämta <img className="icon" src={faArrowRight} alt="Pil höger" />
+                    {getText("link_fetch")} <img className="icon" src={faArrowRight} alt="Pil höger" />
                   </Link>
                 </div>
               </div>
@@ -75,7 +56,7 @@ export default () => {
         <div className="col-12 col-md-3 order-3 order-md-4">
           <Link to="/test">
             <Button theme="primary" className="btn-block">
-              Starta verktyget
+              {getText("button_start")}
             </Button>
           </Link>
         </div>
